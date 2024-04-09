3a32a060e4517a4cb585dd0df8a1fc4debbf699d
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-09 18:48:59 +0000

    translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """Basic flask app"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/')
 def index() -> str:
     """index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
